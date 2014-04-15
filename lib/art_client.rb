fd740729723f6144cfd16e05fe4fa9d9b7db6fa7
Rosie Brigham & Sam Johnson <sam.johnson@fundingcircle.com>
2014-04-15 18:01:41 +0100

    Updated frontend and backend functions between pages

--- lib/art_client.rb
+++ lib/art_client.rb
@@ -4,20 +4,25 @@ require 'arts_gallery_api'
 class ArtClient < Sinatra::Base
   use Rack::Static, :urls => ['/css'], :root => 'public'
 
+
+
   get '/' do
     erb :index
   end
 
   get '/galleries' do
-  erb :galleries
+    erb :galleries
   end
 
   get '/exhibitions' do
-  erb :exhibitions
+    
+    erb :exhibitions
   end
 
-
-  get "/ticket_form" do
+  get "/exhibitions/:exhibition_id/ticket_form" do
+   
     erb :ticket_form
   end
 end
+
+
