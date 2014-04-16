bd5ed32b9cbbedc0cccd47e7347b1f7a0166a8c1
Rosie Brigham & Sam Johnson <sam.johnson@fundingcircle.com>
2014-04-16 18:00:41 +0100

    cleaned up files and added available galleries with exhibitions in the header bar

--- lib/art_client.rb
+++ lib/art_client.rb
@@ -6,7 +6,11 @@ class ArtClient < Sinatra::Base
 
   helpers do
     def gallery_index
-      @gallery_index ||= ArtsGalleryApi::Gallery.new.all["galleries"]
+      gallery_index ||= ArtsGalleryApi::Gallery.new.all["galleries"]
+
+    end
+    def connector
+      connector = ArtsGalleryApi::Gallery.new
     end
   end
 
@@ -15,37 +19,28 @@ class ArtClient < Sinatra::Base
   end
 
   get '/galleries' do
-     connector = ArtsGalleryApi::Gallery.new
-     gallery_index ||= ArtsGalleryApi::Gallery.new.all["galleries"]
-     gallery_index.collect do |gallery_hash| 
-     id = gallery_hash['id']
-     @gallery = connector.retrieve_a_gallery(id)
-     require 'pry'; binding.pry
+      @connector = ArtsGalleryApi::Gallery.new
+   erb :galleries
+ end
+
+ get '/gallery_view/:gallery_id' do
+  id = params[:gallery_id]
+  connector = ArtsGalleryApi::Gallery.new
+  @gallery = connector.retrieve_a_gallery(id)
+  exhibition_index = connector.get_gallery_exhibitions(id)["exhibitions"]
+
+  @exhibitions = exhibition_index.collect do |exhibition|
+    exhibition_number = exhibition["id"]
+    connector.get_exhibition(exhibition_number)
   end
 
+  erb :gallery_view
+ end
 
-     erb :galleries
-   end
+ get "/exhibitions/:exhibition_id/ticket_form" do
 
-   get '/gallery_view/:gallery_id' do
-
-    id = params[:gallery_id]
-    connector = ArtsGalleryApi::Gallery.new
-    @gallery = connector.retrieve_a_gallery(id)
-    exhibition_index = connector.get_gallery_exhibitions(id)["exhibitions"]
-    
-    @exhibitions = exhibition_index.collect do |exhibition|
-      exhibition_number = exhibition["id"]
-      connector.get_exhibition(exhibition_number)
-    end
-
-    erb :gallery_view
-  end
-
-  get "/exhibitions/:exhibition_id/ticket_form" do
-
-    erb :ticket_form
-  end
+  erb :ticket_form
+end
 end
 
 
