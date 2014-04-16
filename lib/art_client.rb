7ad14c6542d1c70c3ae98689baa21f9cd651843c
Rosie Brigham & Sam Johnson <sam.johnson@fundingcircle.com>
2014-04-16 16:07:50 +0100

    Made th code more dynamic and put more of the functionality into the controller rather than the views

--- lib/art_client.rb
+++ lib/art_client.rb
@@ -4,34 +4,40 @@ require 'arts_gallery_api'
 class ArtClient < Sinatra::Base
   use Rack::Static, :urls => ['/css'], :root => 'public'
 
-
+  helpers do
+    def gallery_index
+      @gallery_index ||= ArtsGalleryApi::Gallery.new.all["galleries"]
+    end
+  end
 
   get '/' do
-    gallery = ArtsGalleryApi::Gallery.new
-    @gallery_index = gallery.all["galleries"]
     erb :index
   end
 
   get '/galleries' do
-    erb :galleries
+     connector = ArtsGalleryApi::Gallery.new
+     gallery_index ||= ArtsGalleryApi::Gallery.new.all["galleries"]
+     gallery_index.collect do |gallery_hash| 
+     id = gallery_hash['id']
+     @gallery = connector.retrieve_a_gallery(id)
+     require 'pry'; binding.pry
   end
 
-  get '/exhibitions' do
 
-    erb :exhibitions
-  end
+     erb :galleries
+   end
 
-  get '/gallery_view/:gallery_id' do
-    params[:gallery_id]
-    @id =(params[:gallery_id].to_i - 1)
-    @gallery = ArtsGalleryApi::Gallery.new
-    gallery_index = @gallery.all["galleries"]
-    @galleries = gallery_index.collect do |data_hash|
-      id = data_hash["id"]
-      @gallery.retrieve_a_gallery(id)
+   get '/gallery_view/:gallery_id' do
+
+    id = params[:gallery_id]
+    connector = ArtsGalleryApi::Gallery.new
+    @gallery = connector.retrieve_a_gallery(id)
+    exhibition_index = connector.get_gallery_exhibitions(id)["exhibitions"]
+    
+    @exhibitions = exhibition_index.collect do |exhibition|
+      exhibition_number = exhibition["id"]
+      connector.get_exhibition(exhibition_number)
     end
-
-    @exhibition_details= @gallery.get_gallery_exhibitions(params[:gallery_id])["exhibitions"]
 
     erb :gallery_view
   end
